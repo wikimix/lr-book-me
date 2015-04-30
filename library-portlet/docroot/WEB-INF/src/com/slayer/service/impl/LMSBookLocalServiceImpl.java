f2f7f8bb99a60ef75c051b81c1a344edbcf32be3
Marouen OUESLATI <marouen.oueslati@gmail.com>
2015-04-30 08:56:00 +0200

    Files to commit for service Builder. PS: Other files are generated by liferay

---
+++ library-portlet/docroot/WEB-INF/src/com/slayer/service/impl/LMSBookLocalServiceImpl.java
@@ -0,0 +1,25 @@
+package com.slayer.service.impl;
+
+import com.slayer.service.base.LMSBookLocalServiceBaseImpl;
+
+/**
+ * The implementation of the l m s book local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.slayer.service.LMSBookLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Marouen OUESLATI
+ * @see com.slayer.service.base.LMSBookLocalServiceBaseImpl
+ * @see com.slayer.service.LMSBookLocalServiceUtil
+ */
+public class LMSBookLocalServiceImpl extends LMSBookLocalServiceBaseImpl {
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never reference this interface directly. Always use {@link com.slayer.service.LMSBookLocalServiceUtil} to access the l m s book local service.
+     */
+}
