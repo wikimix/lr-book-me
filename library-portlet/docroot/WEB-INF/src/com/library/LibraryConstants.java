20b73afd7fe8cd6d90174bbc4c5eca4b17ce78bb
Marouen OUESLATI <marouen.oueslati@gmail.com>
2015-05-10 16:07:20 +0200

    move to SDK

--- library-portlet/docroot/WEB-INF/src/com/library/LibraryConstants.java
+++
@@ -1,8 +0,0 @@
-package com.library;
-
-public interface LibraryConstants {
-	static final String ACTION_UPDATE_BOOK = "updateBook";
-	static final String PAGE_UPDATE = "/html/library/update.jsp";
-	static final String PAGE_SUCCESS = "/html/library/success.jsp";
-
-}
