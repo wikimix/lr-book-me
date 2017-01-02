f06d7f2f5472d6d1a3ed954ec75535ceca6eca6d
Marouen OUESLATI <marouen.oueslati@gmail.com>
2017-01-02 10:58:24 +0100

    5.6 Separating the Business Logic

--- library-portlet/src/main/java/com/library/LibraryPortlet.java
+++ library-portlet/src/main/java/com/library/LibraryPortlet.java
@@ -1,7 +1,6 @@
 package com.library;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -10,15 +9,11 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 
-import com.liferay.counter.service.CounterLocalServiceUtil;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.PortletURLFactoryUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
-import com.slayer.model.LMSBook;
-import com.slayer.model.impl.LMSBookImpl;
 import com.slayer.service.LMSBookLocalServiceUtil;
 
 /**
@@ -31,8 +26,7 @@ public class LibraryPortlet extends MVCPortlet {
 
 		String bookTitle = ParamUtil.getString(actionRequest, "bookTitle");
 		String author = ParamUtil.getString(actionRequest, "author");
-
-		insertBook(bookTitle, author);
+		LMSBookLocalServiceUtil.insertBook(bookTitle, author);
 		try {
 			// redirect after insert
 			ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
@@ -53,34 +47,5 @@ public class LibraryPortlet extends MVCPortlet {
 	/**
 	 * @param actionRequest
 	 */
-	private void insertBook(String bookTitle, String author) {
 
-		// 1. Instantiate an empty object of type LMSBookImpl
-		LMSBook lmsBook = new LMSBookImpl();
-
-		// 2. Generate a unique primary key to be set
-		long bookId = 0l;
-		try {
-			bookId = CounterLocalServiceUtil.increment();
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println(" bookId -->>  " + bookId);
-
-		// 3. Set the fields for this object
-		lmsBook.setBookId(bookId);
-		lmsBook.setBookTitle(bookTitle);
-		lmsBook.setAuthor(author);
-		lmsBook.setCreateDate(new Date());
-
-		// 4. Call the Service Layer API to persist the object
-		try {
-
-			lmsBook = LMSBookLocalServiceUtil.addLMSBook(lmsBook);
-
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}
-	}
 }
---
+++ library-portlet/src/main/java/com/slayer/service/impl/LMSBookLocalServiceImpl.java
@@ -0,0 +1,63 @@
+package com.slayer.service.impl;
+
+import java.util.Date;
+
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.slayer.model.LMSBook;
+import com.slayer.model.impl.LMSBookImpl;
+import com.slayer.service.base.LMSBookLocalServiceBaseImpl;
+
+/**
+ * The implementation of the l m s book local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.slayer.service.LMSBookLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Marouen
+ * @see com.slayer.service.base.LMSBookLocalServiceBaseImpl
+ * @see com.slayer.service.LMSBookLocalServiceUtil
+ */
+public class LMSBookLocalServiceImpl extends LMSBookLocalServiceBaseImpl {
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never reference this interface directly. Always use {@link com.slayer.service.LMSBookLocalServiceUtil} to access the l m s book local service.
+     */
+	
+	public LMSBook insertBook(String bookTitle, String author) {
+
+		// 1. Instantiate an empty object of type LMSBookImpl
+		LMSBook lmsBook = new LMSBookImpl();
+
+		// 2. Generate a unique primary key to be set
+		long bookId = 0l;
+		try {
+			bookId = CounterLocalServiceUtil.increment();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(" bookId -->>  " + bookId);
+
+		// 3. Set the fields for this object
+		lmsBook.setBookId(bookId);
+		lmsBook.setBookTitle(bookTitle);
+		lmsBook.setAuthor(author);
+		lmsBook.setCreateDate(new Date());
+
+		// 4. Call the Service Layer API to persist the object
+		try {
+
+			lmsBook = addLMSBook(lmsBook);
+
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		return lmsBook;
+	}
+}
